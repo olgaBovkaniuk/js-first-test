8f04ffb4711acd6b978dbc64d40949b594d4117e
Olga Bovkaniuk <olgabovkaniuk@gmail.com>
2023-09-06 20:54:46 -0600

    modified  index.js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable indent */
 const numberOfFilms = +prompt("сколько фильмов?", "");
 
 const personalMovieDB = {
@@ -8,12 +9,40 @@ const personalMovieDB = {
     private: false
 };
 
-const a = prompt("Один из последних просмотренных фильмов?", ""),
-      b = prompt("На сколько оцените его?", ""), 
-      c = prompt("Один из последних просмотренных фильмов?", ""),
-      d = prompt("На сколько оцените его?", "");
+const questions = ["Один из последних просмотренных фильмов?", "На сколько оцените его?", "Один из последних просмотренных фильмов?", 
+                    "На сколько оцените его?"];
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+let answers = [];
+
+for (let i = 0; i < 2; i++) {
+    let a = prompt("Один из последних просмотренных фильмов?", ""), 
+        b = prompt("На сколько оцените его?", "");
+    if (a !=null && b !=null && a != "" && b != "" && a.length < 50)  {
+        personalMovieDB.movies[a] = b;
+        alert("done");
+    } else {
+        alert("You can't use empty movie name or more than 50 charachters, please try again.");
+        i--;
+    }
+    personalMovieDB.movies[a] = b;
+}
+
+if (personalMovieDB.count < 10) {
+    alert("Просмотрено довольно мало фильмов.");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    alert("Вы классический зритель.");
+} else if (personalMovieDB.count >= 30) {
+    alert("Вы киноман.");
+} else {
+    alert("Произошла ошибка.");
+}
+
+// const a = prompt("Один из последних просмотренных фильмов?", ""),
+//       b = prompt("На сколько оцените его?", ""), 
+//       c = prompt("Один из последних просмотренных фильмов?", ""),
+//       d = prompt("На сколько оцените его?", "");
+
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
 
 console.log(personalMovieDB);
