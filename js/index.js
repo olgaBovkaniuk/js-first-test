9391f259aa70824fb372f12c4df8c5de2d542e79
Olga Bovkaniuk <olgabovkaniuk@gmail.com>
2023-09-07 19:20:01 -0600

    rewrite with functions

--- js/index.js
+++ js/index.js
@@ -1,36 +1,71 @@
 /* eslint-disable indent */
-const numberOfFilms = +prompt("сколько фильмов?", "");
+"use strict";
+
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt("сколько фильмов?", "");
+    while (numberOfFilms == null || numberOfFilms == "" || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt("сколько фильмов?", "");
+    }
+}
+
+start();
 
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
     actors: {},
     genres: {},
-    private: false
+    privat: false
 };
 
-for (let i = 0; i < 2; i++) {
-    let a = prompt("Один из последних просмотренных фильмов?", ""), 
-        b = prompt("На сколько оцените его?", "");
-    if (a !=null && b !=null && a != "" && b != "" && a.length < 50)  {
-        personalMovieDB.movies[a] = b;
-        alert("done");
-    } else {
-        alert("You can't use empty movie name or more than 50 charachters, please try again.");
-        i--;
+function writeYourGenres() {
+    for (let i = 1; i <= 3; i++) {
+        personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
     }
-    personalMovieDB.movies[a] = b;
 }
 
-if (personalMovieDB.count < 10) {
-    alert("Просмотрено довольно мало фильмов.");
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    alert("Вы классический зритель.");
-} else if (personalMovieDB.count >= 30) {
-    alert("Вы киноман.");
-} else {
-    alert("Произошла ошибка.");
+writeYourGenres();
+
+function showMyDB(hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB);
+    }
 }
+
+showMyDB(personalMovieDB[privat]);
+
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        let a = prompt("Один из последних просмотренных фильмов?", ""), 
+            b = prompt("На сколько оцените его?", "");
+        if (a !=null && b !=null && a != "" && b != "" && a.length < 50)  {
+            personalMovieDB.movies[a] = b;
+            alert("done");
+        } else {
+            alert("You can't use empty movie name or more than 50 charachters, please try again.");
+            i--;
+        }
+        personalMovieDB.movies[a] = b;
+    }
+}
+
+rememberMyFilms();
+
+function detectPersonalLever() {
+    if (personalMovieDB.count < 10) {
+        alert("Просмотрено довольно мало фильмов.");
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        alert("Вы классический зритель.");
+    } else if (personalMovieDB.count >= 30) {
+        alert("Вы киноман.");
+    } else {
+        alert("Произошла ошибка.");
+    }
+}
+
+detectPersonalLever();
 
 // const a = prompt("Один из последних просмотренных фильмов?", ""),
 //       b = prompt("На сколько оцените его?", ""), 
